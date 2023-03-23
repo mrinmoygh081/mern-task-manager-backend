cb599b51c623e86babc5a74cd17bccd93cc53eec
Mrinmoy Ghosh <mrinmoygh081@gmail.com>
2023-03-23 17:28:47 +0530

    mern-task-backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+// Routes
+const taskRoutes = require("./routes/taskRoute");
+
+// Middleware configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: [
+      "http://10.12.1.151:3000",
+      "https://todo-deploy-mern.onrender.com",
+    ],
+  })
+);
+app.use("/api/v1/tasks", taskRoutes);
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server running on port " + port);
+    });
+  })
+  .catch((err) => console.error(err));
